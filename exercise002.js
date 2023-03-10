694de9474904bdec277fd166740c592093d786e2
mikeyhw1 <70017382+mikeyhw1@users.noreply.github.com>
2023-03-10 19:14:36 +0000

    feat: finished exer 2 extension

--- exercise002.js
+++ exercise002.js
@@ -11,9 +11,33 @@ const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
 // fetchData(jsonTypicode);
 
+
+// task 1:
+// try {
+//   const response = await fetch(jsonTypicode)
+//   const fetchData = await response.json()
+//   console.log(fetchData)
+// } catch (error) {
+//   console.log(error)
+// }
+
+
+// extension:
+const extensionApi = "https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2"
 try {
-  const response = await fetch(jsonTypicode)
+  const header = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": "40ad9574d32841c5b39336cfd6c0967a"
+    },
+  }
+
+  const response = await fetch(extensionApi, header)
+  console.log("~~~~~~ response:")
+  console.log(response)
   const fetchData = await response.json()
+  console.log("~~~~~~ fetchData:")
   console.log(fetchData)
 } catch (error) {
   console.log(error)
